140c46ba55f9ed584602f774ee5dd5dbff90aebe
iagofontes <iagofontes@hotmail.com>
2023-05-09 18:40:35 -0300

    fix cors in controllers

--- src/main/java/br/com/iagofontes/currencyconverter/core/convert/controller/ConvertController.java
+++ src/main/java/br/com/iagofontes/currencyconverter/core/convert/controller/ConvertController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/convert")
 public class ConvertController {
 
--- src/main/java/br/com/iagofontes/currencyconverter/core/symbol/controller/SymbolController.java
+++ src/main/java/br/com/iagofontes/currencyconverter/core/symbol/controller/SymbolController.java
@@ -6,14 +6,15 @@ import br.com.iagofontes.currencyconverter.core.symbol.mapper.SymbolResponseMapp
 import br.com.iagofontes.currencyconverter.core.symbol.service.SymbolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/symbols")
 public class SymbolController {
 
